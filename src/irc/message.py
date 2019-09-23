755a45a301a272cf61f96378beace0d8ad1ce2dc
olagood <>
2019-09-23 03:58:44 +0300

    Updating the license note, the copyright years and the description message

--- src/irc/message.py
+++ src/irc/message.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python3
 # coding=utf-8
 
-# Message parser for IRC
+# Parse messages recieved by the IRC server and pack them in
+# variables suitable for usage by the bot's functions.
 
 '''
-Copyright (C) 2018 drastik.org
+Copyright (C) 2018-2019 drastik.org
 
 This file is part of drastikbot.
 
-Drastikbot is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, version 3 only.
 
-Drastikbot is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Drastikbot. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 
