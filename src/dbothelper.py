bd117b36568984b329701941c68f35528b9571aa
olagood <derezzed@protonmail.com>
2022-07-24 15:53:27 +0300

    Added functions to return weekday and month strings from numbers

--- src/dbothelper.py
+++ src/dbothelper.py
@@ -1,3 +1,32 @@
 def is_ascii_cl(x, y):
     """Is x an ascii caseless match for y ?"""
     return x.lower() == y.lower()
+
+
+def get_day_str(num):
+    return {
+        0: "Mon",
+        1: "Tue",
+        2: "Wed",
+        3: "Thu",
+        4: "Fri",
+        5: "Sat",
+        6: "Sun"
+    }[num]
+
+
+def get_month_str(num):
+    return {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+    }[num]
